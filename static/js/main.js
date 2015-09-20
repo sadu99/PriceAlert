22b57c72afdbc25d050cb96da75c2204c3e018de
Felix Wei <felixwei@hotmail.ca>
2015-09-19 23:51:13 -0400

    Changed styling

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
-    // $(".intro").fadeIn(2000);
-    // setTimeout(function(){ $(".overlay").fadeOut(1000); }, 2000);
+    $(".intro").fadeIn(2000);
+    setTimeout(function(){ $(".overlay").fadeOut(1000); }, 3000);
       $.ajax({
             url: '/tickers',
             type: 'GET',
             success: function(response) {
                 console.log(response);
                 responses = JSON.parse(response);
-                $( "#end-date" ).autocomplete({
+                $( "#index-value" ).autocomplete({
 			      source: responses
 			    });
             },
@@ -15,7 +15,7 @@ $( document ).ready(function() {
                 console.log(error);
             }
         });
-      
+
  	var indexArray = [];
     $('#submit-button').click(function(event){
         // Stop the form from actually submitting.        
@@ -23,12 +23,29 @@ $( document ).ready(function() {
         
         // Capture the entered name and put it into the placeholder.
         var endDate = $('#end-date').val();
-        var ticker = "testing";
-        var start = "testing";
-        var end = "testing";
+        var ticker = $("#index-value").val();
+        var start = "2014-01-01";
+        var end = endDate;
+        window.location.href = "/submit?ticker=" + ticker + "&start=" + start + "&end=" + end;
+		// $.ajax({
+  //           url: '/submit',
+  //           data: {
+  //           	ticker: ticker,
+  //           	start: start,
+  //           	end: end
+  //           },
+  //           type: 'GET',
+  //           success: function(response) {
+  //               console.log(response);
+  //           },
+  //           error: function(error) {
+  //               console.log(error);
+  //           }
+  //       });
+
 
        
-        // window.location.href = "/submit?ticker=" + ticker + "&start=" + start + "&end=" + end;
+       
       
     });
 
